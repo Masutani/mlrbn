45e880867d390f9452628a9df92ae23b5c50ee06
Osamu Masutani <masutani_itlab@live.jp>
2019-02-13 20:32:21 +0900

    Add new measure (multilabel.fpr)

--- R/measures.mlrbn.R
+++ R/measures.mlrbn.R
@@ -25,3 +25,51 @@ multilabel.ppv1 = makeMeasure(
   fun = ppv1fun
 )
 
+#' internal function for measure fpr
+#' 
+fprfun = function(task, model, pred, feats, extra.args) {
+    truth = getPredictionTruth(pred)
+    response = getPredictionResponse(pred)
+    numerator = rowSums(!truth & response) # FP
+    denominator = rowSums(!truth) # FP + TN
+    fpr = mean(numerator / denominator, na.rm = TRUE)
+    #    matchn = apply(match, MARGIN = c(1, 2), as.numeric)
+    #    ppv1 <- sum(rowSums(matchn) > 0) / nrow(matchn)
+    return(fpr)
+}
+
+#' measure false positive rate for multilabel
+#' @rdname measures
+#' @format none
+#' @export
+multilabel.fpr = makeMeasure(
+  id = "multilabel.fpr", name = " false positive rate in multiple label",
+  properties = c("classif.multi", "multilabel", "req.pred", "req.truth"),
+  minimize = TRUE, best = 0, worst = 1,
+  fun = fprfun
+)
+
+
+#' internal function for measure fpr
+#' 
+fpr1fun = function(task, model, pred, feats, extra.args) {
+    truth = getPredictionTruth(pred)
+    response = getPredictionResponse(pred)
+    numerator = sum(rowSums(!truth & response) >0 ) # FP
+    denominator = sum(rowSums(!truth) >0) # FP + TN
+    fpr1 = mean(numerator / denominator, na.rm = TRUE)
+    #    matchn = apply(match, MARGIN = c(1, 2), as.numeric)
+    #    ppv1 <- sum(rowSums(matchn) > 0) / nrow(matchn)
+    return(fpr1)
+}
+
+#' measure false positive rate for multilabel
+#' @rdname measures
+#' @format none
+#' @export
+multilabel.fpr1 = makeMeasure(
+  id = "multilabel.fpr", name = " false positive rate at leastone in multiple label",
+  properties = c("classif.multi", "multilabel", "req.pred", "req.truth"),
+  minimize = TRUE, best = 0, worst = 1,
+  fun = fpr1fun
+)
